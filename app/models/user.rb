9f0dd6bd536a6e5af09d620373ab062a37dcfdc6
Brian Dunlap <briandunlap589@gmail.com>
2015-11-22 13:17:15 -0500

    Add devise user validations, remove previous bcrypt validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-# has_secure_password
 
-has_many :posts
-has_many :comments
-
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username, :email
+  has_many :posts
+  has_many :comments
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
 end
