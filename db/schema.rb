38e393f7df07a63220b19d79caf3f621204375af
Brian Dunlap <briandunlap589@gmail.com>
2015-11-22 10:28:57 -0500

    Remove password digest column from user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102194357) do
+ActiveRecord::Schema.define(version: 20151122152411) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -30,11 +30,10 @@ ActiveRecord::Schema.define(version: 20151102194357) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "email",           null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string   "username",   null: false
+    t.string   "email",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
